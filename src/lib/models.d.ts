8f481e143df24474a12520ad2538930f21faf557
AslamThachapalli <aslam.develop912@gmail.com>
2024-08-10 17:00:06 +0530

    designed temporary models

--- src/lib/models.d.ts
+++ src/lib/models.d.ts
@@ -32,3 +32,61 @@ interface Address {
     address: string;
     landmark: string;
 }
+
+interface Product {
+    id: string;
+    name: string;
+    categoryId: string;
+    price: number;
+    stockQuantity: number;
+}
+
+interface Category {
+    id: string;
+    categoryName: string;
+}
+
+interface Cart {
+    id: string;
+    userId: string;
+    cartItems: CartItem[];
+    quantity: number;
+}
+
+interface CartItem {
+    productId: string;
+    quantity: number;
+    price: number;
+}
+
+type OrderStatus = 'created' | 'attempted' | 'paid';
+
+interface Order {
+    id: string;
+    userId: string;
+    providerOrderId: string;
+    status: OrderStatus;
+    addressId: string;
+    totalAmount: number;
+    createdAt: number;
+    updatedAt: number;
+    orderItems: OrderItem[];
+}
+
+interface OrderItem {
+    productId: string;
+    quantity: number;
+    price: number;
+}
+
+type PaymentStatus = "created" | "authorized" | "captured" | "refunded" | "failed";
+
+interface Payment {
+    id: string;
+    userId: string;
+    providerPaymentId: string;
+    orderId: string;
+    status: PaymentStatus;
+    createdAt: number;
+    updatedAt: number;
+}
